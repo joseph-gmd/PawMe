c22c30e1df102edd0b90355623318c392c0e85a0
joseph-gmd <josephgerarddiaz@gmail.com>
2022-07-19 14:32:55 +0800

    Added contents on home page

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -7,7 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  slideOpts = {
+    slidesPerView: 2.2,
+  };
+
+  slideOpts1 = {
+    slidesPerView: 1.3,
+  };
+
+  popularDogs: any[] = [
+    {id: 1, name: 'Archie', breed:'Pomeranian', gender: 'Male', age: 3, size: 'small', img: 'assets/dogs/pom1.jfif'},
+    {id: 2, name: 'Luna', breed:'Shiba Inu', gender: 'Female', age: 4, size: 'medium', img: 'assets/dogs/shiba1.jpg'},
+    {id: 3, name: 'Bogart', breed:'American Bully', gender: 'Male', age: 2, size: 'medium', img: 'assets/dogs/bully1.jpg'},
+    {id: 4, name: 'Eugene', breed:'Shiba Inu', gender: 'Male', age: 3, size: 'medium', img: 'assets/dogs/shiba2.jpg'}
+  ];
+
   constructor() { }
+
+  findShiba(popularDogs: any []) {
+    return popularDogs.filter(popularDogs => popularDogs.breed == 'Shiba Inu');
+  }
 
   ngOnInit() {
   }
