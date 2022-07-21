c87beaaaa5da8d4f25bac1f2fa654934492bb241
joseph-gmd <josephgerarddiaz@gmail.com>
2022-07-21 12:37:14 +0800

    Added form validation (+ regex) for Registration

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { Validators, FormControl, FormGroup, FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -8,15 +10,42 @@ import { Router } from '@angular/router';
 })
 export class RegisterPage implements OnInit {
 
-  constructor(private router : Router) { }
+  RegisterForm: FormGroup;
+
+  fnamefromuser: any;
+  addressfromuser: any;
+  cnumfromuser: any;
+  emailfromuser: any;
+  passwordfromuser: any;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+
+    this.RegisterForm = new FormGroup ({
+      fname: new FormControl('', [
+        Validators.required,
+      ]),
+      address: new FormControl('', [
+        Validators.required,
+      ]),
+      cnum: new FormControl('', [
+        Validators.required,
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+      ]),
+    });
+
   }
 
   userLogin() {
-    this.router.navigate(['/login'])
-  }
+    this.router.navigate(['/login']);
+  };
   home() {
-    this.router.navigate(['/tabs'])
-  }
+    this.router.navigate(['/tabs']);
+  };
 }
