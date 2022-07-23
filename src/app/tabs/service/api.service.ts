d911805ef036cef7f8012440a5bc77a4790065ac
joseph-gmd <josephgerarddiaz@gmail.com>
2022-07-23 16:44:39 +0800

    Added elements in popularDogs[] array

--- src/app/tabs/service/api.service.ts
+++ src/app/tabs/service/api.service.ts
@@ -9,7 +9,8 @@ export class ApiService {
     {id: 1, name: 'Archie', breed:'Pomeranian', gender: 'Male', age: 3, size: 'small', location: 'Lipa City', img: 'assets/dogs/pom1.jfif'},
     {id: 2, name: 'Luna', breed:'Shiba Inu', gender: 'Female', age: 4, size: 'medium', location: 'Batangas City', img: 'assets/dogs/shiba1.jpg'},
     {id: 3, name: 'Bogart', breed:'American Bully', gender: 'Male', age: 2, size: 'medium', location: 'Batangas City', img: 'assets/dogs/bully1.jpg'},
-    {id: 4, name: 'Eugene', breed:'Shiba Inu', gender: 'Male', age: 3, size: 'medium', location: 'Rosario', img: 'assets/dogs/shiba2.jpg'}
+    {id: 4, name: 'Eugene', breed:'Shiba Inu', gender: 'Male', age: 3, size: 'medium', location: 'Rosario', img: 'assets/dogs/shiba2.jpg'},
+    {id: 5, name: 'Chowder', breed:'Chow Chow', gender: 'Female', age: 5, size: 'medium', location: 'Lipa City', img: 'assets/dogs/chowder.jpg'}
   ];
 
   constructor() { }
